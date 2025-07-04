7c9c9852b1ecbb3dfaf03266e5f1188ea106c05a
Simarjot Singh (Monga) <simar5244@gmail.com>
2025-07-03 22:21:47 -0500

    cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://seloraa.com',
+  output: 'static',
   integrations: [tailwind(), react()],
   typescript: {
     ignoreBuildErrors: true
+  },
+  server: {
+    host: true,
+    port: 3000
   }
 });
